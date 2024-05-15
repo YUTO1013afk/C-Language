49485d000e299a6d437752715d2f70ef0891012d
優人 田島 <yourentiandao304@gmail.com>
2024-05-15 11:27:10 +0900

    Commit

---
+++ C/章末問題5-2.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+    int i = 0;
+    int n;
+    int sum = 0;
+
+    printf("®”’l‚ð“ü—Í‚µ‚Ä‚­‚¾‚³‚¢-> ");
+    scanf("%d",&n);
+    while (i <= n) {
+        sum = sum + i;
+        i++;
+    }
+    printf("%d\n",sum);
+    return 0;
+}
