8dfa6a148da6a3353464f21c05f6c3126855c140
優人 田島 <yourentiandao304@gmail.com>
2024-05-21 11:51:29 +0900

    Commit

---
+++ C/章末問題11-1.cpp
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+	int i;
+	char* name[5];
+	for (i = 0; i < 5; i++) {
+		name[i] = (char*)malloc(10 * sizeof(char));
+		printf("name[%d]F", i);
+		scanf("%s", name[i]);
+	}
+	for (i = 0; i < 5; i++) {
+		printf("%s\n", name[i]);
+	}
+
+	for (i = 0; i < 5; i++) free(name[i]);
+		return 0;
+}
